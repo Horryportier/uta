8932e1a01ef0c92efba8637e1593b9d96336eb36
horry <horry@pop-os.localdomain>
2023-08-28 17:44:26 +0200

    made better help

--- src/api.rs
+++ src/api.rs
@@ -1,4 +1,4 @@
-use std::{fs, io::stdin, process::Command};
+use std::{fs, io::stdin, process::Command, sync::Mutex};
 
 use mpvipc::{Mpv, SeekOptions};
 use rand::Rng;
@@ -10,18 +10,13 @@ use crate::{env::get_env_mpv_args, Error};
 #[derive(Debug)]
 pub struct Player {
     pub data: Data,
-    pub mpv: Option<Mpv>,
+    pub mpv: Mutex<Option<Mpv>>,
 }
 
 #[derive(Serialize, Deserialize, SerializePartial, Debug)]
 pub struct Data {
-    pub is_runing: bool,
-    pub log: bool,
     pub mpv_args: Vec<String>,
     pub url: Option<String>,
-
-    pub previous_video: (),
-    pub time: usize,
 }
 
 impl Player {
@@ -37,18 +32,10 @@ impl Player {
         .to_vec();
 
         def_args.append(&mut get_env_mpv_args());
-
-        let mpv = match Mpv::connect(socket) {
-            Ok(mpv) => Some(mpv),
-            Err(_) => None,
-        };
+        let mpv = Mutex::new(mpvipc::Mpv::connect("/tmp/mpvsocket").ok());
 
         let data = Data {
-            is_runing: false,
-            log: false,
             mpv_args: def_args,
-            previous_video: (),
-            time: 0,
             url: None,
         };
 
@@ -73,58 +60,50 @@ impl Player {
         let cmd = Command::new("mpv")
             .args(&self.data.mpv_args)
             .arg(final_url)
-            .spawn()
-            .map_err(|e| Error::IoErr(e))?;
+            .spawn()?;
         info!("start cmd {:?}", cmd);
         Ok(())
     }
 
     pub fn kill(&self) -> Result<(), Error> {
-        let cmd = Command::new("pkill")
-            .arg("mpv")
-            .output()
-            .map_err(|e| Error::IoErr(e))?;
+        let cmd = Command::new("pkill").arg("mpv").output()?;
         info!("kill cmd {:?}", cmd);
         Ok(())
     }
     pub fn next(&self) -> Result<(), Error> {
-        self.mpv
-            .clone()
-            .unwrap()
-            .next()
-            .map_err(|e| Error::MpvError(e))?;
+        self.mpv.lock().unwrap().as_ref().unwrap().next()?;
         Ok(())
     }
     pub fn prev(&self) -> Result<(), Error> {
-        self.mpv
-            .clone()
-            .unwrap()
-            .prev()
-            .map_err(|e| Error::MpvError(e))?;
+        self.mpv.lock().unwrap().as_ref().unwrap().prev()?;
         Ok(())
     }
 
     pub fn seek(&self, time: f64, opt: SeekOptions) -> Result<(), Error> {
-        self.mpv
-            .clone()
-            .unwrap()
-            .seek(time, opt)
-            .map_err(|e| Error::MpvError(e))?;
+        self.mpv.lock().unwrap().as_ref().unwrap().seek(time, opt)?;
         Ok(())
     }
 
     pub fn toggle(&self) -> Result<(), Error> {
-        self.mpv
-            .clone()
-            .unwrap()
-            .toggle()
-            .map_err(|e| Error::MpvError(e))?;
+        self.mpv.lock().unwrap().as_ref().unwrap().toggle()?;
         Ok(())
+    }
+
+    pub fn is_paused(&self) -> Result<bool, Error> {
+        Ok(self
+            .mpv
+            .lock()
+            .unwrap()
+            .as_ref()
+            .unwrap()
+            .get_property::<bool>("pause")?)
     }
 
     pub fn print(&self) -> Result<(), Error> {
         let curr: f64 = self
             .mpv
+            .lock()
+            .unwrap()
             .as_ref()
             .unwrap()
             .get_property("playback-time")
@@ -132,6 +111,8 @@ impl Player {
             .floor();
         let len: f64 = self
             .mpv
+            .lock()
+            .unwrap()
             .as_ref()
             .unwrap()
             .get_property("duration")
@@ -141,10 +122,11 @@ impl Player {
         let text = format!(
             "{procent}/100% | {}",
             self.mpv
+                .lock()
+                .unwrap()
                 .as_ref()
                 .unwrap()
-                .get_property::<String>("media-title")
-                .map_err(|e| Error::MpvError(e))?
+                .get_property::<String>("media-title")?
         );
         println!("{}", text);
         Ok(())
@@ -152,54 +134,60 @@ impl Player {
 
     pub fn loop_single(&self) -> Result<(), Error> {
         self.mpv
-            .clone()
+            .lock()
             .unwrap()
-            .set_loop_file(mpvipc::Switch::Toggle)
-            .map_err(|e| Error::MpvError(e))?;
+            .as_ref()
+            .unwrap()
+            .set_loop_file(mpvipc::Switch::Toggle)?;
         Ok(())
     }
 
     pub fn loop_playlist(&self) -> Result<(), Error> {
         self.mpv
-            .clone()
+            .lock()
             .unwrap()
-            .set_loop_playlist(mpvipc::Switch::Toggle)
-            .map_err(|e| Error::MpvError(e))?;
+            .as_ref()
+            .unwrap()
+            .set_loop_playlist(mpvipc::Switch::Toggle)?;
         Ok(())
     }
 
     pub fn rand(&self) -> Result<(), Error> {
         let count: usize = self
             .mpv
+            .lock()
+            .unwrap()
             .as_ref()
             .unwrap()
-            .get_property("playlist-count")
-            .map_err(|e| Error::MpvError(e))?;
+            .get_property("playlist-count")?;
         let rng = rand::thread_rng().gen_range(1..count);
         self.mpv
+            .lock()
+            .unwrap()
             .as_ref()
             .unwrap()
-            .playlist_play_id(rng)
-            .map_err(|e| Error::MpvError(e))?;
+            .playlist_play_id(rng)?;
         Ok(())
     }
 
     pub fn volume(&self, mut volume: f64) -> Result<(), Error> {
         volume = volume.clamp(0., 100.);
         self.mpv
-            .clone()
+            .lock()
             .unwrap()
-            .set_volume(volume, mpvipc::NumberChangeOptions::Absolute)
-            .map_err(|e| Error::MpvError(e))?;
+            .as_ref()
+            .unwrap()
+            .set_volume(volume, mpvipc::NumberChangeOptions::Absolute)?;
         Ok(())
     }
     pub fn get_volume(&self) -> Result<(), Error> {
         let volume: f64 = self
             .mpv
-            .clone()
+            .lock()
             .unwrap()
-            .get_property("volume")
-            .map_err(|e| Error::MpvError(e))?;
+            .as_ref()
+            .unwrap()
+            .get_property("volume")?;
         println!("{volume}");
         Ok(())
     }
@@ -207,15 +195,17 @@ impl Player {
     pub fn downland(&self, opt_url: Option<&str>) -> Result<(), Error> {
         let mut url = self
             .mpv
-            .clone()
+            .lock()
             .unwrap()
-            .get_property_string("filename")
-            .map_err(|e| Error::MpvError(e))?;
+            .as_ref()
+            .unwrap()
+            .get_property_string("filename")?;
 
         match opt_url {
             None => {}
             Some(u) => url = u.into(),
         }
+        // UTA_DOWNLAND args for yt-dlp 
         let yt_dlp_args = match option_env!("UTA_DOWNLAND") {
             None => "",
             Some(a) => a,
@@ -226,11 +216,7 @@ impl Player {
         args.push(&full_url);
         println!("{full_url}");
         println!("{:?}", args);
-        let cmd = Command::new("yt-dlp")
-            .args(args)
-            .arg(url)
-            .spawn()
-            .map_err(|e| Error::IoErr(e))?;
+        let cmd = Command::new("yt-dlp").args(args).arg(url).spawn()?;
 
         info!("start cmd {:?}", cmd);
         Ok(())
@@ -245,8 +231,8 @@ impl Player {
             return Ok(());
         }
 
-        let file = String::from_utf8(fs::read(list_file).map_err(|e| Error::IoErr(e))?)
-            .map_err(|e| Error::UtfErr(e))?;
+        let file = String::from_utf8(fs::read(list_file)?)?;
+
         let lines = file.split("\n").collect::<Vec<&str>>();
         let entiers = lines
             .iter()
@@ -271,11 +257,11 @@ impl Player {
             input_string.trim().parse::<usize>()
         };
 
-        let final_choice = choice().map_err(|e| Error::IntErr(e))?;
+        let final_choice = choice()?;
 
         let url = entiers.get(final_choice).unwrap().get(1).unwrap();
 
-        let p = self.clone();
+        let p = self;
 
         p.kill()?;
 
@@ -285,34 +271,5 @@ impl Player {
         p.start(Some(b))?;
 
         Ok(())
-    }
-}
-
-pub fn is_runing() -> bool {
-    let cmd = Command::new("pgrep").arg("mpv").output();
-    match cmd {
-        Err(_) => false,
-        Ok(child) => {
-            if child.stdout.is_empty() {
-                return false;
-            }
-            true
-        }
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::*;
-
-    #[test]
-    fn test_runing() {
-        let res = is_runing();
-        assert_eq!(true, res)
-    }
-    #[test]
-    fn test_not_runing() {
-        let res = is_runing();
-        assert_eq!(false, res)
     }
 }
--- src/args.rs
+++ src/args.rs
@@ -1,7 +1,9 @@
-use crate::api::is_runing;
-use crate::{api::Player, Error};
+use std::fmt::Display;
+use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
 
-use crate::utils::print_help;
+use crate::{api::Player, Error};
 
 #[derive(Debug)]
 pub enum Arg {
@@ -11,7 +13,6 @@ pub enum Arg {
 
     HelpFlag,
 
-    Start,
     Kill,
     Next,
     Prev,
@@ -21,14 +22,84 @@ pub enum Arg {
     Rand,
     Downland,
 
+    Runnig,
+
     Print,
 
-    Other,
+    Other(String),
+
+    None,
+}
+
+impl Display for Arg {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let text = match &self {
+            Arg::Runnig => format!("-rn will print if mpv is paused"),
+            Arg::Kill => format!("-k --kill will kill mpv procceses"),
+            Arg::Seek(_) => format!("-s --seek given number betwwen 0..100\n\twill jump to that point in video"),
+            Arg::Next => format!("-n --next will jump to next entry in playlist"),
+            Arg::Prev => format!("-p --prev will jump to prev entry in playlist"),
+            Arg::Volume(_)=> format!("-v --volume when passed number will change volume\n\t to that number clamped betwwen 0..100\n\t if not passed will print current volume"),
+            Arg::Toogle => format!("-t --toogle toggels betwwen paused/not paused"),
+            Arg::LoopPlaylist => format!("-lp --loop_playlist sets playlist to looping mode"),
+            Arg::Loop => format!("-l --loop sets current entry to looping mode"),
+            Arg::Link(_) => format!("passing link will start mpv with socket at `/tmp/mpvsocket`"),
+            Arg::Other(_) => format!(""),
+            Arg::HelpFlag => format!("-h --help shows help"),
+            Arg::Rand => format!("-r -rand jumps to random entry in playlist"),
+            Arg::Print => format!("--print prints current entry name"),
+            Arg::Downland => format!("-d if prowided link will downland that video using yt-dlp\n\t if not and mpv is runnig then will downland current entry as video"),
+            Arg::None => format!("passing non arguments will print\n\t list of playlist from UTA_LIST_FILE env if set")
+        };
+        writeln!(f, "{}", text)
+    }
 }
 
 #[derive(Debug)]
 pub struct ArgsManger {
     pub args: Vec<Arg>,
+}
+
+impl Display for ArgsManger {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let mut text: Vec<String> = vec![
+            "\t\tUta".into(),
+            "uta is an basic mpv music player".into(),
+            "you can use your own local playlist or yt url".into(),
+            "ENV:".into(),
+            "UTA_DOWNLAND args for yt-dlp".into(),
+            "UTA_LIST_FILE when set will read from file and give you optinos of playlist to start"
+                .into(),
+            "file has to be in format of".into(),
+            "playlist1 URL1".into(),
+            "playlist2 URL2".into(),
+            "ARGS:".into(),
+        ];
+
+        let args = vec![
+            Arg::None,
+            Arg::Link("".into()),
+            Arg::Runnig,
+            Arg::Kill,
+            Arg::Seek(0),
+            Arg::Next,
+            Arg::Prev,
+            Arg::Toogle,
+            Arg::LoopPlaylist,
+            Arg::Loop,
+            Arg::Rand,
+            Arg::Downland,
+            Arg::Runnig,
+            Arg::Print,
+            Arg::Other("".into()),
+            Arg::HelpFlag,
+            Arg::Volume(None),
+        ];
+
+        args.iter().for_each(|f| text.push(format!("{}", f)));
+
+        write!(f, "{}", text.join("\n"))
+    }
 }
 
 impl ArgsManger {
@@ -42,15 +113,14 @@ impl ArgsManger {
         for (i, arg) in args.iter().enumerate() {
             a.push(match arg.as_str() {
                 "-h" | "--help" => Arg::HelpFlag,
-                "--start" => Arg::Start,
                 "-k" | "--kill" => Arg::Kill,
                 "-n" | "--next" => Arg::Next,
                 "-p" | "--prev" => Arg::Prev,
                 "-t" | "--toogle" => Arg::Toogle,
                 "-r" | "--rand" => Arg::Rand,
                 "-l" | "--loop" => Arg::Loop,
-                "-lp" | "--loop_playlist" => Arg::LoopPlaylist,
-                "-d"  => Arg::Downland,
+                "--lp" | "--loop_playlist" => Arg::LoopPlaylist,
+                "-d" => Arg::Downland,
                 "--print" => Arg::Print,
                 "-v" | "--volume" => Arg::Volume(match args.get(i + 1) {
                     None => None,
@@ -62,53 +132,57 @@ impl ArgsManger {
                         .parse::<usize>()
                         .unwrap_or(0),
                 ),
-                arg if is_numeric(arg) => Arg::Other,
-                //arg if is_valid_link(&arg) => Arg::Link(arg.into()), impl later
-                _ => Arg::Link(arg.into()),
+                arg if is_numeric(arg) => Arg::Other(arg.into()),
+                arg if is_valid_link(&arg) => Arg::Link(arg.into()),
+                "-rn" => Arg::Runnig,
+                _ => Arg::Other(arg.into()),
             });
         }
         a
     }
 
     pub fn execute(&self) -> Result<(), Error> {
-        let mut player: Player = Player::new().map_err(|e| Error::MpvError(e))?;
+        let mut player: Player = Player::new()?;
 
-        if !is_runing() {
-            let link = self.args.iter().find_map(|f| match f {
-                Arg::Link(link) => Some(link.clone()),
-                _ => None,
-            });
-            match link {
-                None => Err(Error::ExecuteErr(
-                    "no link ink args\n mvp is not runing so you need to provide url".into(),
-                )),
-                Some(_) => Ok(())
-            }?;
-        }
-
-        
         if self.args.len() == 0 {
             player.chose_from_list()?;
         }
+
         info!("Player =>{:?}", player);
 
-        for (i,arg) in self.args.iter().enumerate() {
+        for (i, arg) in self.args.iter().enumerate() {
             match arg {
-                Arg::Link(link) => player.data.url = Some(link.into()),
-                Arg::HelpFlag => print_help(),
-                Arg::Start => {
+                Arg::None => {}
+                Arg::HelpFlag => match self.args.get(i + 1) {
+                    None => println!("{}", self),
+                    Some(a) => {
+                        println!("{}", a);
+                        return Ok(());
+                    }
+                },
+                Arg::Link(url) => {
+                    player.data.url = Some(url.into());
                     player.start(None)?;
-                    if player.mpv.is_none() {
-                        player.mpv = Some(
-                            mpvipc::Mpv::connect("/tmp/mpvsocket")
-                                .map_err(|e| Error::MpvError(e))?,
-                        );
+                    if player.mpv.lock().unwrap().is_none() {
+                        sleep(Duration::from_secs(1));
+                        player.mpv = Some(mpvipc::Mpv::connect("/tmp/mpvsocket")?).into();
                     }
                 }
                 Arg::Kill => player.kill()?,
                 Arg::Next => player.next()?,
                 Arg::Prev => player.prev()?,
-                Arg::Toogle => player.toggle()?,
+                Arg::Toogle => {
+                    player.toggle()?;
+                    sleep(Duration::from_secs(1));
+                    println!(
+                        "mpv is {}",
+                        match player.is_paused()? {
+                            true => "runnig",
+                            false => "not runnig",
+                        }
+                    )
+                }
+
                 Arg::Print => player.print()?,
                 Arg::Seek(dest) => player.seek(
                     dest.to_string().parse::<f64>().unwrap_or(0.),
@@ -122,16 +196,23 @@ impl ArgsManger {
                     None => player.get_volume()?,
                 },
                 Arg::Downland => {
-                    let url: Option<&str> = match self.args.get(i+1) {
+                    let url: Option<&str> = match self.args.get(i + 1) {
                         None => None,
-                        Some(a) => match  a {
+                        Some(a) => match a {
                             Arg::Link(l) => Some(l),
-                            _ => None
-                        }
+                            _ => None,
+                        },
                     };
                     player.downland(url)?;
-                },
-                _ => {}
+                }
+                Arg::Runnig => println!(
+                    "mpv is {}",
+                    match player.is_paused()? {
+                        true => "runnig",
+                        false => "not runnig",
+                    }
+                ),
+                Arg::Other(a) => println!("{} is an invalid argument\n use \"uta -h\" for help", a),
             }
         }
 
@@ -146,9 +227,15 @@ pub fn is_numeric(input: &str) -> bool {
     }
 }
 
-#[allow(dead_code)]
 fn is_valid_link(url: &str) -> bool {
-    let s: Vec<&str> = url.split("/").collect();
-    info!("{:?}", s);
-    true
+    if Path::new(url).exists() {
+        return true;
+    }
+
+    let mut is_valid = false;
+    let valid_links_prefixes = ["https://youtube.com/", "youtube.com/"];
+    valid_links_prefixes.iter().for_each(|f| {
+        is_valid = url.contains(f);
+    });
+    is_valid
 }
